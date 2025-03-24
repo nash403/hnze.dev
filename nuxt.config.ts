88a13fa89cb11791501701e29a72f497256b1eb3
Honoré Nintunze <nintu.lab@gmail.com>
2025-03-24 17:21:32 -0400

    chore: set local ssl config and pkg deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,54 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+const caName = ['localhost', '0.0.0.0'].includes(process.env.HOST as string)
+  ? 'rootCA'
+  : process.env.HOST
+
+const https = process.env.WEBAPP_LOCAL_USE_SSL
+  ? {
+    key: process.env.CAROOT + `/${`${caName}-key`}.pem`,
+    cert: process.env.CAROOT + `/${`${caName}`}.pem`,
+  }
+  : undefined
+
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/content',
-  ],
-  devtools: { enabled: true },
   future: {
     compatibilityVersion: 4,
   },
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: '2025-10-03',
+
+  // Env variables
+  runtimeConfig: {
+    public: {
+      TOTO: '',
+    },
+    FOO: process.env.FOO || '',
+  },
+
+  // Installed modules
+  modules: [
+    '@nuxt/content',
+  ],
+
+  // Other configurations
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true,
+    },
+  },
+  devServer: {
+    port: +(process.env.PORT ?? 3000),
+    https,
+  },
+
+  vite: {
+    server: {
+      https,
+      hmr: {
+        protocol: process.env.WEBAPP_LOCAL_USE_SSL ? 'wss' : 'ws'
+      }
+    }
+  }
 })
