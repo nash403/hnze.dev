30829e423fd144b12482a4af3b3f1b439181f617
Honoré Nintunze <nintu.lab@gmail.com>
2025-11-05 17:57:50 +0100

    fixed nav bar navigation and responsive issues

--- app/components/HNavigationBar.vue
+++ app/components/HNavigationBar.vue
@@ -18,31 +18,18 @@ const navItems: Ref<NavItem[]> = ref([
     label: 'Career',
   },
 ])
-
-const menuOpened = ref(false)
-
-const toggleMenuOpen = () => (menuOpened.value = !menuOpened.value)
-const closeMenu = () => {
-  menuOpened.value = false
-}
 </script>
 
 <template>
-  <div
-    aria-hidden="true"
-    class="fixed inset-0 z-30 bg-black/40"
-    :class="menuOpened ? 'flex md:hidden' : 'hidden'"
-    @click="closeMenu"
-  ></div>
-  <header class="sticky top-0 z-40 w-full backdrop-blur-[1.25rem] backdrop-saturate-[180%] select-none has-[:checked]:bg-base-100">
+  <header class="sticky top-0 z-40 w-full backdrop-blur-[1.25rem] select-none not-has-[:checked]:[mask-image:linear-gradient(to_bottom,black_90%,transparent_100%)] after:absolute after:right-0 after:bottom-0 after:left-0 after:h-[1px] after:bg-gradient-to-r after:from-transparent after:to-transparent after:blur-sm after:content-[''] not-has-[:checked]:after:via-primary has-[:checked]:bg-base-200 supports-[backdrop-filter]:backdrop-saturate-[180%]">
     <div
       class="relative mx-auto flex max-w-6xl items-center justify-between pr-4 sm:py-0 sm:pr-8 md:px-8"
     >
-      <!-- Menu toggle -->
+      <!-- Rest of the code remains the same -->
       <label
-        class="peer group relative cursor-pointer border-r-2 border-r-base-content-400 p-2 outline-none md:hidden "
-        @click="toggleMenuOpen"
+        class="peer group relative cursor-pointer border-r-2 border-r-base-content px-2 py-3 outline-none md:hidden "
       >
+        <!-- @click="toggleMenuOpen" -->
         <input
           type="checkbox"
           :aria-label="$t('app.navbar.toggle_menu_label')"
@@ -50,20 +37,20 @@ const closeMenu = () => {
         />
         <div
           aria-hidden="true"
-          class="h-0.5 w-4 rounded bg-base-content transition group-hover:bg-primary peer-checked:translate-y-[0.33rem] peer-checked:rotate-45"
+          class="h-0.5 w-5 rounded bg-base-content transition group-hover:bg-primary peer-checked:translate-y-[0.33rem] peer-checked:rotate-45"
         ></div>
         <div
           aria-hidden="true"
-          class="mt-2 h-0.5 w-4 rounded bg-base-content transition group-hover:bg-primary peer-checked:translate-y-[-0.33rem] peer-checked:-rotate-45"
+          class="mt-2 h-0.5 w-5 rounded bg-base-content transition group-hover:bg-primary peer-checked:translate-y-[-0.33rem] peer-checked:-rotate-45"
         ></div>
       </label>
 
       <!-- Logo & Let's meet CTA -->
-      <div class="mr-auto ml-2 flex items-center space-x-10 md:ml-0">
+      <div class="mr-auto ml-2 flex items-center space-x-10 py-[4px] md:ml-0">
         <NuxtLink
           :to="localePath('index')"
           :title="$t('app.link_go_home')"
-          class="-no-glow flex items-center"
+          class="flex items-center"
         >
           <Icon
             name="hnze:hnze-logo"
@@ -83,7 +70,7 @@ const closeMenu = () => {
 
       <!-- Site navigation links -->
       <nav
-        class="invisible absolute inset-x-0 top-full z-50 ml-auto border-b border-b-gray-300/40 bg-base-100 pr-4 pl-8 opacity-0 duration-300 ease-linear peer-has-[:checked]:visible peer-has-[:checked]:opacity-100 sm:px-8 md:visible md:relative md:border-b-0 md:bg-transparent md:px-0 md:opacity-100"
+        class="invisible absolute inset-x-0 top-full z-50 ml-auto border-b border-b-gray-300/40 bg-base-200 pr-4 pl-8 opacity-0 duration-300 ease-linear peer-has-[:checked]:visible peer-has-[:checked]:opacity-100 sm:px-8 md:visible md:relative md:border-b-0 md:bg-transparent md:px-0 md:opacity-100"
       >
         <ul class="flex flex-col gap-x-2 pb-4 md:flex-row md:items-center md:justify-between md:p-0 lg:mr-16 lg:gap-x-5">
           <li
@@ -100,6 +87,7 @@ const closeMenu = () => {
               <template #default="slot">
                 <span :class="['group flex items-center rounded p-1 lg:px-3 lg:py-2', { 'text-base-content-800 md:bg-primary-100': slot?.isActive }]">
                   <Icon
+                    v-if="nav.icon"
                     :name="nav.icon"
                     class="mr-2 hidden size-5 items-center text-sm opacity-75 transition group-hover:opacity-100 md:text-base xl:inline-flex"
                     :class="{ 'opacity-100': slot?.isActive }"
