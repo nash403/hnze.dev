f674edcd483b84caa44f7a51af5d698783e865a1
Honoré Nintunze <nintu.lab@gmail.com>
2025-11-05 11:06:16 +0100

    add custom error page to be customized

---
+++ app/pages/error.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+defineProps({
+  error: Object as () => NuxtError,
+})
+
+const handleError = () => clearError({ redirect: '/' })
+</script>
+
+<template>
+  <!-- eslint-disable @intlify/vue-i18n/no-raw-text -->
+  <div>
+    <h2>{{ error?.statusCode }}</h2>
+    <button
+      type="button"
+      class="btn btn-primary"
+      @click="handleError"
+    >
+      Go back to the homepage
+    </button>
+  </div>
+</template>
