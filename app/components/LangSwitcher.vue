31a4a0b821ac4290f17ada799bdc30d14a33c78f
Honoré Nintunze <nintu.lab@gmail.com>
2025-11-14 11:02:46 +0100

    feat: better text decoration for current locale display

--- app/components/LangSwitcher.vue
+++ app/components/LangSwitcher.vue
@@ -18,7 +18,7 @@ const switchLocalePath = useSwitchLocalePath()
         :to="switchLocalePath(_locale.code)"
         :class="['mx-2 link pb-[2px] no-underline not-[.router-link-active]:h-link-glow hover:[&.router-link-active]:text-primary', {
           'cursor-not-allowed': _locale.disabled,
-          'font-bold underline decoration-2': _locale.code == locale,
+          'font-bold underline decoration-base-content-600 decoration-2 hover:decoration-primary': _locale.code == locale,
         }]"
         :disabled="_locale.disabled"
         :label="_locale.name"
