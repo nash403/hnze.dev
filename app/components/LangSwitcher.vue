dd3ddc6fe060637654fa459930cf37959eb57220
Honoré Nintunze <nintu.lab@gmail.com>
2025-12-01 18:48:55 +0100

    refactor: modernize locale switcher component

--- app/components/LangSwitcher.vue
+++ app/components/LangSwitcher.vue
@@ -1,28 +1,28 @@
 <script lang="ts" setup>
 const { locale, locales } = useI18n()
-const switchLocalePath = useSwitchLocalePath()
 </script>
 
 <template>
   <div class="flex items-center justify-between">
     <template
-      v-for="(_locale, i) in locales"
-      :key="_locale.code"
+      v-for="(registeredLocale, i) in locales"
+      :key="registeredLocale.code"
     >
       <div
         v-if="i"
         class="self-stretch border-r-2 border-base-content-600"
       ></div>
-      <NuxtLink
-
-        :to="switchLocalePath(_locale.code)"
+      <SwitchLocalePathLink
+        :locale="registeredLocale.code"
         :class="['mx-2 link pb-0.5 no-underline not-[.router-link-active]:h-link-glow hover:[&.router-link-active]:text-primary', {
-          'cursor-not-allowed': _locale.disabled,
-          'font-bold underline decoration-base-content-600 decoration-2 hover:decoration-primary': _locale.code == locale,
+          'cursor-not-allowed': registeredLocale.disabled,
+          'font-bold underline decoration-base-content-600 decoration-2 hover:decoration-primary': registeredLocale.code == locale,
         }]"
-        :disabled="_locale.disabled"
-        :label="_locale.name"
-      >{{ _locale.name }}</NuxtLink>
+        :disabled="registeredLocale.disabled"
+        :label="registeredLocale.name"
+      >
+        {{ registeredLocale.name }}
+      </SwitchLocalePathLink>
     </template>
   </div>
 </template>
