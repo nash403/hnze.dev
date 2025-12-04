88f53b0837fcbf6fe9eeed026e70f4f83b2f41b6
Honoré Nintunze <nintu.lab@gmail.com>
2025-12-04 08:51:11 +0100

    feat: colorize logo cloud when focused or active

--- app/components/LogoCloud.vue
+++ app/components/LogoCloud.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import type { IconifyIconName } from '#shared/types'
 import type { RouteLocationRaw } from 'vue-router'
+import { NuxtLink } from '#components'
 
 export interface LogoCloudItem {
   name: string
@@ -11,48 +12,44 @@ export interface LogoCloudItem {
   imgWidth?: string
 }
 
-withDefaults(defineProps<LogoCloudItem>(), { target: '_blank', imgWidth: '60' })
+const props = withDefaults(defineProps<LogoCloudItem>(), { target: '_blank', imgWidth: '60' })
+
+const wrapperAttrs = computed(() => {
+  if (props.to) {
+    return {
+      'to': props.to,
+      'target': props.target,
+      'external': true,
+      'class': 'inline-flex items-center justify-center transition-all duration-300',
+      'title': props.name,
+      'aria-label': props.name,
+    }
+  }
+
+  return {
+    class: 'inline-flex items-center justify-center transition-all duration-300',
+    title: props.name,
+  }
+})
 </script>
 
 <template>
-  <NuxtLink
-    v-if="to"
-    :to="to"
-    :target="target"
-    external
-    class="inline-flex items-center justify-center transition-all duration-300"
-    :title="name"
-    :aria-label="name"
+  <component
+    :is="to ? NuxtLink : 'span'"
+    v-bind="wrapperAttrs"
+    class="group"
   >
     <NuxtImg
       v-if="src"
       :src="src"
-      class="opacity-50 grayscale transition-all hover:opacity-100 hover:filter-none"
+      class="opacity-50 grayscale transition-all group-hover:opacity-100 group-hover:filter-none group-focus:opacity-100 group-focus:filter-none group-active:opacity-100 group-active:filter-none"
       :width="imgWidth"
       quality="90"
     />
     <Icon
       v-else-if="icon"
       :name="icon"
-      class=" opacity-50 grayscale transition-all duration-300 hover:opacity-100 hover:drop-shadow-lg hover:grayscale-0 hover:filter-none "
+      class=" opacity-50 grayscale transition-all duration-300 group-hover:opacity-100 group-hover:grayscale-0 group-hover:filter-none group-focus:opacity-100 group-focus:filter-none group-active:opacity-100 group-active:filter-none"
     />
-  </NuxtLink>
-  <span
-    v-else
-    class="inline-flex items-center justify-center transition-all duration-300"
-    :title="name"
-  >
-    <NuxtImg
-      v-if="src"
-      :src="src"
-      class=" opacity-50 grayscale transition-all hover:opacity-100 hover:filter-none"
-      :width="imgWidth"
-      quality="90"
-    />
-    <Icon
-      v-else-if="icon"
-      :name="icon"
-      class="opacity-50 grayscale transition-all hover:opacity-100 hover:filter-none "
-    />
-  </span>
+  </component>
 </template>
